baa172e2946e66f46cbe444400f976a063d52a12
shunsukeyamaguchi <yamagutisyunsuke@gmail.com>
2026-01-21 22:51:11 +0900

    Day9: toggle message using if statement

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const button = document.getElementById("hello-btn");
 const message = document.getElementById("message");
 
+// let isShown = false;
+
 button.addEventListener("click", () => {
-  message.textContent = "Hello JavaScript!";
+  // if (isShown) {
+  //   message.textContent = "";
+  //   button.textContent = "表示する";
+  // } else {
+  //   message.textContent = "Hello JavaScript!";
+  //   button.textContent = "非表示にする";
+  // }
+
+  if (message.textContent == "") {
+    message.textContent = "Hell JavaScript";
+    button.textContent = "非表示にする";
+  } else {
+    message.textContent = "";
+    button.textContent = "表示する";
+  }
+
+
+  // isShown = !isShown
 });
