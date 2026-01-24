de9bba31694b4c90badf9fb846853de751eec4ff
shunsukeyamaguchi <yamagutisyunsuke@gmail.com>
2026-01-24 19:05:31 +0900

    Day11: refactor logs using objects

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+const button = document.getElementById("hello-btn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+  if (message.textContent == "") {
+    message.textContent = "Hello JavaScript";
+    button.textContent = "非表示する";
+  } else {
+    message.textContent = "";
+    button.textContent = "表示する";
+  }
+})
+
 const logs = [
-  "Day1 : 環境変数",
-  "Day2 : HTML基礎",
-  "Day3 : CSS基礎",
-  "Day4 : Flexbox",
-  "Day5 : レスポンシブ",
+  {day:"Day1", title:"環境変数"},
+  {day:"Day2", title:"HTML基礎"},
+  {day:"Day3", title:"CSS基礎"},
+  {day:"Day4", title:"Flexbox"},
+  {day:"Day5", title:"レスポンシブ"}
 ];
 
 const container = document.querySelector(".container");
 
-logs.push("Day6 : JavaScript基礎");
+logs.push({day:"Day6", title:"JavaScript基礎"});
 
 logs.forEach((log) => {
   const div = document.createElement("div");
   div.className = "box";
-  div.textContent = log;
+  div.textContent = `${log.day} : ${log.title}` ;
   container.appendChild(div);
 })
